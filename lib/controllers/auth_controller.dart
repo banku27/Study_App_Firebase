2e28d4ce3ded6f6aff3e2aeceeb1e62972299bf2
banku27 <pankajparihar123321@gmail.com>
2022-08-25 18:35:52 +0530

    google firebase authentication

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:study_app_firebase/firebase_ref/references.dart';
 
 class AuthController extends GetxController {
   @override
@@ -7,9 +11,45 @@ class AuthController extends GetxController {
     super.onReady();
   }
 
+  late FirebaseAuth _auth;
+  final _user = Rxn<User>();
+  late Stream<User?> _authStateChanges;
+
   void initAuth() async {
     await Future.delayed(const Duration(seconds: 2));
+    _auth = FirebaseAuth.instance;
+    _authStateChanges = _auth.authStateChanges();
+    _authStateChanges.listen((User? user) {
+      _user.value = user;
+    });
     navigateToIntroduction();
+  }
+
+  signInWithGoogle() async {
+    final GoogleSignIn _googleSignIn = GoogleSignIn();
+    try {
+      GoogleSignInAccount? account = await _googleSignIn.signIn();
+      if (account != null) {
+        final _authAccount = await account.authentication;
+        final _credential = GoogleAuthProvider.credential(
+          idToken: _authAccount.idToken,
+          accessToken: _authAccount.accessToken,
+        );
+
+        await _auth.signInWithCredential(_credential);
+        await saveUser(account);
+      }
+    } on Exception catch (e) {
+      Get.snackbar('error', e.toString());
+    }
+  }
+
+  saveUser(GoogleSignInAccount account) {
+    userReference.doc(account.email).set({
+      "email": account.email,
+      "name": account.displayName,
+      "profilepic": account.photoUrl,
+    });
   }
 
   void navigateToIntroduction() {
--- lib/firebase_ref/references.dart
+++ lib/firebase_ref/references.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
 final fireStore = FirebaseFirestore.instance;
+final userReference = fireStore.collection("users");
 final questionPaperReference = fireStore.collection('questionPapers');
 
 DocumentReference questionReference({
